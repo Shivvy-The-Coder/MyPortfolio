cc098fd2e8886626450571d56182551306482e0a
Shivvy-The-Coder <krshivam455@gmail.com>
2025-01-08 00:59:21 +0530

    navbar progress -responsive 90% complete, theme and colour has to be updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import Logo from "/Images/Logo.png"
 import Button from '../Buttons/Button';
 import { BiSearchAlt } from "react-icons/bi";
 import { AiOutlineUser } from "react-icons/ai";
-import { FaRegSquareCaretDown } from "react-icons/fa6";
+import { FaAnglesDown } from "react-icons/fa6";
 
 
 const Actions=[
@@ -84,13 +84,39 @@ const Navbar = () => {
 
 
                 {/* Ending Section beins here */}
-                <div className=' flex xxs:justify-end  sm:justify-center font-Poppin text-[90%] xxs:max-w-[100%]'>
+                <div className='cursor-pointer flex xxs:justify-end  sm:justify-center font-Poppin text-[90%] xxs:max-w-[100%] h-[5vh] group'>
                         <div className='font-serif flex flex-cols-3 gap-2 items-center justify-center text-xl '>
-                                <div className='sm:hidden'><FaRegSquareCaretDown/></div>
+                                <div className='
+                                        relative group
+                                        transition-all 
+                                        duration-200'>
+
+                                <FaAnglesDown className='
+                                        
+                                        sm:hidden
+                                        relative group
+                                        transition-all 
+                                        duration-400
+                                        group-hover:rotate-180'/>
+                                </div>
+                                <div className='absolute hidden group-hover:block w-[15%] rounded-md bg-transparent border-white top-[60px] right-[30px] '>
+                                        <ul >
+                                                        {
+                                                        Actions.map((data)=>(
+                                                                <li className='text-black inline-block w-full rounded-md p- text-[70%]  text-center  bg-white mt-1 cursor-pointer'
+                                                                 key={data.id}  
+                                                                 > 
+                                                                 {data.name}
+                                                               </li>
+                                                         ))}
+                                                
+                                        </ul>
+                                </div>
                                 <div><BiSearchAlt/></div>
                                 <div> <AiOutlineUser/> </div>
                         </div>
                 </div>
+
                 {/* Ending section ends here */}
                 
             </div>
