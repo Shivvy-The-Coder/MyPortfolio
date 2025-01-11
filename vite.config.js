b2b4f43900d494fc71983054dcf4ab9560de39c1
Shivvy-The-Coder <krshivam455@gmail.com>
2025-01-11 12:38:02 +0530

    codes updated

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// vite.config.js
-export default {
-  build: {
-    rollupOptions: {
-      external: ['@rollup/rollup-linux-x64-gnu'],
-    },
-  },
-}
+export default defineConfig({
+  plugins: [react()],
+})
