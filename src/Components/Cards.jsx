8b7da1347441e02e4e9f4a3db3ca2cdfca2ec7f4
Shivvy-The-Coder <krshivam455@gmail.com>
2025-01-15 14:30:45 +0530

    codes updated for Project Section

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,54 +1,49 @@
-import React from 'react'
+import React from 'react';
 
-
-const Cards = ({name , link , img , desc,delay,animation}) => {
+const Cards = ({ name, link, img, desc, delay, animation, customClass,customeimg }) => {
   return (
-    <div> 
-
+    <div>
+      <div>
+        <div
+          data-aos-delay={delay}
+          data-aos={animation}
+          className={`flex flex-col font-Poppin p-3 mb-[5vh] sm:scale-90 overflow-auto md:w-[80%] sm:w-[70%] w-[70%] h-[70%] mx-auto
+            sm:rounded-lg sm:shadow-md sm:shadow-gray-500/50 backdrop-blur-xl border-[5px]
+            border-white/10 shadow-md rounded-sm cursor-pointer gap-2 shadow-white hover:shadow-none
+            ${customClass || 'hover:scale-[110%] hover:translate-y-[-3vh]'} 
+          `}
+        >
+          {/* Heading Section */}
           <div>
-                <div 
-                data-aos-delay={delay}
-                data-aos={animation}
-                className='flex flex-col font-Poppin  p-3  mb-[5vh]  sm:scale-90  overflow-auto md:w-[80%] sm:w-[100%] w-[70%] h-[70%] mx-auto
-                                sm:rounded-lg sm:shadow-md sm:shadow-gray-500/50
-                backdrop-blur-xl
-                border-[5px]
-                border-white/10 shadow-md rounded-sm 
-                cursor-pointer
-                gap-2
-                shadow-white
-                hover:shadow-none
-                '>    
-                      {/*For HEading Section  */}
-                      <div>
-                        <h1
-                       
-                        className='text-2xl sm:text-4xl text-center text-shadow-black text-yellow-500 font-bold '
-                        >{name}</h1>
-                      </div>
-
-                      {/* for image section */}
-                      <div
-                        className='p-10 flex justify-center w-[80%] sm:w-[80%] mx-auto bg-white/10 rounded-lg '
-                      >
-                        <img 
-                        className='transition-transform duration-1000 ease-in-out transform hover:scale-[125%]  hover:translate-y-[-3vh] contrast-200 saturate-200 '
-                        src={img} alt="" />
-                      </div>
-
-                      {/* for information */}
-                      <div>
-                        <p className='sm:text-sm text-[12px] text-white 
-                                  text-center text-opacity-70 shadow-md transition-transform duration-1000 ease-in-out transform hover:scale-[110%] text-shadow-black
-                                  bg-gray-500/20 rounded-md hover:bg-yellow-400/10  '>
-                          {desc}
-                        </p>
-                      </div>
-                </div>
+            <h1
+              className="text-2xl sm:text-4xl text-center text-shadow-black text-yellow-500 font-bold"
+            >
+              {name}
+            </h1>
           </div>
 
+          {/* Image Section */}
+          <div className={`flex justify-center
+                ${customeimg||'p-10  w-[80%] sm:w-[80%]'}
+                 mx-auto bg-white/10 rounded-lg`}>
+            <img
+              className={`transition-transform duration-1000 ease-in-out transform 
+                ${customClass ? '' : 'hover:scale-[125%] hover:translate-y-[-3vh]'} contrast-150 saturate-100`}
+              src={img}
+              alt=""
+            />
+          </div>
+
+          {/* Information Section */}
+          <div>
+            <p className="sm:text-sm text-[12px] text-white text-center text-opacity-70 shadow-md transition-transform duration-1000 ease-in-out transform bg-gray-500/20 rounded-md hover:bg-yellow-400/10">
+              {desc}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Cards;
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,41 +1,77 @@
-import React from 'react'
-import Cards from '../Cards'
+import React from 'react';
+import Cards from '../Cards';
+import Shopsy from '/Images/Shopsy.png';
+import Dice from '/Images/DiceGame.png';
+import BIT from '/Images/BIT.png';
+
+const ProjectList = [
+  {
+    id: 1,
+    name: 'Dice Game',
+    image: Dice,
+    link: '#',
+    desc: 'A simple but quite engaging and fun game of Dices where you can score all you want unlimitedly',
+  },
+  {
+    id: 2,
+    name: 'Git',
+    image: Shopsy,
+    link: '#',
+    desc: 'Online Shopping Website build properly on tailwind CSs and ',
+  },
+  {
+    id: 3,
+    name: 'Git-Hub',
+    image: BIT,
+    link: '#',
+    desc: 'GitHub is a cloud-based platform for hosting Git repositories, offering collaboration, issue tracking, CI/CD integration, and open-source project hosting, simplifying teamwork and code sharing for developers.',
+  },
+];
+
 const Projects = () => {
   return (
     <div>
-        <div>
-            
-            {/* Intro Section */}
-
-            <div className=' text-white font-Poppin text-center  max-w-[90%] mx-auto mt-20 '>
-            <div>
-                <p data-aos="fade-up"
-                className='text-yellow-500'
-                >Making an Impact with Every Project</p>
-            </div>
-            <div>
-                <h1 data-aos="fade-up" 
-                 className='
-                 text-3xl font-bold'
-                 style={{ textShadow: "1px 1px 8px  rgba(60, 61, 61, 0.863)" }}
-                 >PROJECT'S SECTION</h1>
-            </div>
-
-            <div>
-                <p data-aos="fade-up" 
-                 className='text-yellow-500'>Dedicated to creating projects that leave a lasting positive impression.</p>
-            </div>
-            </div>
-
-            {/* Main Projects Section */}
-            <div>
-                  <Cards
-                  name="DICE GAME USING REACT JS"
-                  />
-            </div>
+      <div>
+        {/* Intro Section */}
+        <div className="text-white font-Poppin text-center max-w-[90%] mx-auto mt-20">
+          <div>
+            <p data-aos="fade-up" className="text-yellow-500">
+              Making an Impact with Every Project
+            </p>
+          </div>
+          <div>
+            <h1
+              data-aos="fade-up"
+              className="text-3xl font-bold"
+              style={{ textShadow: '1px 1px 8px  rgba(60, 61, 61, 0.863)' }}
+            >
+              PROJECT'S SECTION
+            </h1>
+          </div>
+          <div>
+            <p data-aos="fade-up" className="text-yellow-500">
+              Dedicated to creating projects that leave a lasting positive impression.
+            </p>
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default Projects
+        {/* Main Projects Section */}
+        <div>
+          {ProjectList.map((data) => (
+            <div key={data.id} className="max-w-[70%] mx-auto max-h-[90vh]">
+              <Cards
+                customClass="!transition-transform !hover:translate-x-2 !hover:translate-y-2 !hover:scale-100"
+                customeimg="sm:w-[70%] w-[100%] sm:p-[10] p-[1]"
+                name={data.name}
+                img={data.image}
+                desc={data.desc}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
